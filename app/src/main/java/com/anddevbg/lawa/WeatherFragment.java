b35102a1bede410af4dfa1c85bca66ec9721d34a
anddevbg <anddevbg@gmail.com>
2015-06-07 16:48:18 +0300

    Initial commit.

---
+++ app/src/main/java/com/anddevbg/lawa/WeatherFragment.java
@@ -0,0 +1,23 @@
+package com.anddevbg.lawa;
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class WeatherFragment extends Fragment {
+
+    public WeatherFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_weather, container, false);
+    }
+}
